933aa8a651b55de988cd6c447ae734b967cb40bd
dinhviethai10 <145420234+dinhviethai10@users.noreply.github.com>
2025-12-13 16:40:35 +0700

    Create index.js to export product detail components

---
+++ frontend/src/components/ProductDetail/index.js
@@ -0,0 +1,5 @@
+export { default as ProductImageGallery } from './ProductImageGallery';
+export { default as ProductInfo } from './ProductInfo';
+export { default as BiddingSection } from './BiddingSection';
+export { default as TopBiddersSection } from './TopBiddersSection';
+export { default as CommentsSection } from './CommentsSection';
